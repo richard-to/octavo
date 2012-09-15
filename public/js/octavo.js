4a977e773b12a898e8573a05e6f38caf1c6bb5e1
richard-to <gohn76@gmail.com>
2012-09-15 15:29:15 -0800

    Integrate backbone model/collection to manage posts

--- public/js/octavo.js
+++ public/js/octavo.js
@@ -2,14 +2,40 @@
     
     var config = window.OCTAVO_CONFIG
 
+    var PostModel = Backbone.Model.extend({
+        urlRoot: '/api/posts'
+    })
+
+    var PostCollection = Backbone.Collection.extend({
+        model: PostModel,
+        url: '/api/posts'
+    })
+
+    var posts = new PostCollection()
+
+    var PostView = Backbone.View.extend({
+        render: function() {
+
+        }
+    })
+
     var OctavoRouter = Backbone.Router.extend({
         routes: {
-            ":post": "post_action"
+            ":post_id": "post_action"
         },
-        post_action: function(post){
-            $.getJSON('/api/posts/' + post, function(data) {
-                $('.content .pad').html(data.content)
-            }, 'json');
+        post_action: function(post_id){
+            var post = posts.get(post_id)
+            if(!post) {
+                post = new PostModel({id:post_id})
+                post.fetch({
+                    success: function(){
+                        $('.content .pad').html(post.get('content'))
+                        posts.add([post])
+                    }
+                })
+            } else {
+                $('.content .pad').html(post.get('content'))    
+            }
         }
     })
 
