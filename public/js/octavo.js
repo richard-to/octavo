42f2d010990d7394e3194f2776db7c713e842822
richard-to <gohn76@gmail.com>
2012-09-15 16:20:44 -0800

    Integrate initial view with models and router

--- public/js/octavo.js
+++ public/js/octavo.js
@@ -1,49 +1,75 @@
- $('document').ready(function(){
-    
-    var config = window.OCTAVO_CONFIG
+var config = window.OCTAVO_CONFIG
 
-    var PostModel = Backbone.Model.extend({
-        urlRoot: '/api/posts'
-    })
-
-    var PostCollection = Backbone.Collection.extend({
-        model: PostModel,
-        url: '/api/posts'
-    })
-
-    var posts = new PostCollection()
-
-    var PostView = Backbone.View.extend({
-        render: function() {
-
-        }
-    })
-
-    var OctavoRouter = Backbone.Router.extend({
-        routes: {
-            ":post_id": "post_action"
-        },
-        post_action: function(post_id){
-            var post = posts.get(post_id)
-            if(!post) {
-                post = new PostModel({id:post_id})
-                post.fetch({
-                    success: function(){
-                        $('.content .pad').html(post.get('content'))
-                        posts.add([post])
-                    }
-                })
-            } else {
-                $('.content .pad').html(post.get('content'))    
-            }
-        }
-    })
-
-    var router = new OctavoRouter()
-    Backbone.history.start({pushState:true, silent:true})
-
-    $('.menu a').click(function(e){
-        e.preventDefault()
-        router.navigate(this.href.replace(config.site_url, ''), {trigger: true})
-    })
+var PostModel = Backbone.Model.extend({
+    urlRoot: '/api/posts'
 })
+
+var PostCollection = Backbone.Collection.extend({
+    model: PostModel,
+    url: '/api/posts'
+})
+
+var PostView = Backbone.View.extend({
+    initalize: function(){
+        _.bindAll(this, 'render', 'setPost')
+    },
+    render: function() {
+        $el.html(this.content)
+    },
+    setPost: function(postId){
+        var view = this
+        var post = this.collection.get(postId)
+        if(!post) {
+            view.model = new PostModel({id:postId})
+            view.model.fetch({
+                success: function(){
+                    view.collection.add([view.model])
+                    view.render()
+                }
+            })
+        } else {
+            this.model = post
+            this.render()            
+        }
+    }
+})
+
+var AppView = Backbone.View.extend({
+    initialize: function() {
+        _.bindAll(this, 'render', 'viewPost')
+        this.collection = new PostCollection()
+    },
+    render: function(){
+        this.postView = new PostView({
+            el: this.$('.content .pad'), 
+            collection: this.collection
+        })
+    },
+    events: {
+        'click .menu a': 'viewPost'
+    },
+    viewPost: function(e){
+        e.preventDefault()
+        this.router.navigate(e.target.href.replace(config.site_url, ''), {trigger: true})        
+    }
+})
+
+var OctavoRouter = Backbone.Router.extend({
+    initialize: function(options){
+        this.app = options.app
+        this.app.router = this
+    },
+    routes: {
+        ":post_id": "postAction"
+    },
+    postAction: function(post_id){
+        this.app.postView.setPost(post_id)
+    }
+})
+
+var app = new AppView()
+var router = new OctavoRouter({
+    app:app
+})
+
+Backbone.history.start({pushState:true, silent:true})
